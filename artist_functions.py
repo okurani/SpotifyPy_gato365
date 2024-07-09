2d1cf50d2ffe86d8f956fe2c3ab87cc70bb82f8b
miahodges10 <mia.hodges@me.com>
2024-07-09 11:47:57 -0700

    Created get_artist_projects function.

--- artist_functions.py
+++ artist_functions.py
@@ -48,10 +48,34 @@ def get_artists(queries=None, ids=None, access_token=None):
     })
     return df
 
+def get_artist_projects(query=None, id=None, access_token=None, limit=20, offset=0):
+    if query:
+        access_token = get_spotify_access_token(client_id, client_secret)
+        search_results = search_spotify([query], access_token)
+        id = search_results[0]['id'] if search_results else None
+    if not id:
+        raise ValueError("Artist ID must be provided or found through a search query.")
+    
+    url = f'https://api.spotify.com/v1/artists/{id}/albums'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    params = {
+        'include_groups': 'album,single',
+        'market': 'US',
+        'limit': limit,
+        'offset': offset
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch albums: {response.text}")
+    
+    albums = response.json().get('items', [])
+    df = pd.DataFrame.from_records(albums)
+    df = df[['id', 'name', 'release_date', 'release_date_precision']].rename(columns={'id': 'album_id', 'name': 'album_name'}).drop_duplicates()
+    return df
+
 client_id = os.getenv('SPOTIFY_CLIENT_ID')
 client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 access_token = get_spotify_access_token(client_id, client_secret)
 
-queries = ["Taylor Swift", "Jordan Davis"]
-artist_df = get_artists(queries = queries, access_token=access_token)
-print(artist_df)
+artist_project_df = get_artist_projects(query = "Taylor Swift", access_token=access_token)
+print(artist_project_df)
