17eef09f7089e8f21251fb3e864eaab569bec92b
miahodges10 <mia.hodges@me.com>
2024-07-15 09:59:04 -0700

    I added search_spotify to authorize.py.

--- authorize.py
+++ authorize.py
@@ -14,15 +14,15 @@ def get_spotify_access_token(client_id, client_secret):
         raise Exception(f"Failed to get access token: {auth_response.text}")
     return auth_response.json()['access_token']
 
-def search_spotify(queries, access_token):
+def search_spotify(queries, search_type, access_token):
     search_results = []
     for query in queries:
         search_url = 'https://api.spotify.com/v1/search'
         headers = {'Authorization': f'Bearer {access_token}'}
-        params = {'q': query, 'type': 'artist', 'limit': 1}  # Set limit to 1 for demo purposes
+        params = {'q': query, 'type': search_type, 'limit': 1}  # Set limit to 1 for demo purposes
         response = requests.get(search_url, headers=headers, params=params)
         if response.status_code == 200:
-            search_results.extend(response.json()['artists']['items'])
+            search_results.extend(response.json()[f'{search_type}s']['items'])
         else:
             print(f"Failed to search for {query}: {response.text}")
     return search_results
